ba419ba073503988c0dc24a8ef54eb0c0e724842
lakshay17 <lakshayjain01lj@gmail.com>
2025-05-25 11:53:42 +0530

    signin form component

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -154,7 +154,7 @@ export default function SignInForm() {
 
             <CardFooter className="flex justify-center py-4">
                 <p className="text-sm text-default-600">
-                    Don't have an account?{" "}
+                    Don&apos;t have an account?{" "}
                     <Link
                         href="/sign-up"
                         className="text-primary hover:underline font-medium"
